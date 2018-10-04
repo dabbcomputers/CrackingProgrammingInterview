753028599c6fabe8b5faad640c2c8e33554998f1
Amritpal singh <amrsing@microsoft.com>
2018-10-04 09:08:06 +0530

    Approximation Square Root function

---
+++ Fundamentals/Fundamentals/ApproximationSquareRoot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Fundamentals
+{
+	class ApproximationSquareRoot
+	{
+		public static double FindSquareRoot(int N)
+		{
+			int c = 1;
+			while(c*c <= N)
+			{
+				c *= 2;
+			}
+			int a = 0;
+			while(c != 1)
+			{
+				c = c / 2;
+				if ((a + c) * (a + c) <= N)
+					a = a + c;
+			}
+			return a;
+		}
+	}
+}
